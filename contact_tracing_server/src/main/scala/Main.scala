d9339deb5b7f040d2c8d0a97aaffe1a7b6503d38
massimilianonigro <massimiliano.nigro@mail.polimi.it>
2021-09-21 16:29:36 +0200

    Semi-working version of akka backend

--- contact_tracing_server/src/main/scala/Main.scala
+++ contact_tracing_server/src/main/scala/Main.scala
@@ -1,25 +1,54 @@
-import akka.Done
-import akka.actor.ActorSystem
+import Main.system.dispatcher
+import akka.{Done, NotUsed}
+import akka.actor.{ActorSystem, Props, RootActorPath}
+import akka.stream.RestartSettings
 import akka.stream.alpakka.mqtt.{
   MqttConnectionSettings,
+  MqttMessage,
   MqttQoS,
   MqttSubscriptions
 }
-import akka.stream.alpakka.mqtt.scaladsl.{MqttMessageWithAck, MqttSource}
-import akka.stream.scaladsl.Source
+import akka.stream.alpakka.mqtt.scaladsl.{
+  MqttMessageWithAck,
+  MqttSink,
+  MqttSource
+}
+import akka.stream.scaladsl.{Flow, RestartSource, Sink, Source}
+import akka.util.Timeout
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 
-import scala.concurrent.Future
+import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Success}
 
 object Main extends App {
+  private def wrapWithAsRestartSource[M](
+      source: => Source[M, Future[Done]]
+  ): Source[M, Future[Done]] = {
+    val fut = Promise[Done]
+    RestartSource
+      .withBackoff(
+        RestartSettings(
+          100.millis,
+          3.seconds,
+          randomFactor = 0.2d
+        )
+      ) { () =>
+        source.mapMaterializedValue(mat => fut.completeWith(mat))
+      }
+      .mapMaterializedValue(_ => fut.future)
+  }
+
   implicit val system: ActorSystem = ActorSystem("ContactTracingServer")
+  implicit val timeout: Timeout = Timeout(5, TimeUnit.SECONDS)
   val connectionSettings = MqttConnectionSettings(
-    "tcp://localhost:1883",
+    "tcp://0.tcp.ngrok.io:13999",
     "backend",
     new MemoryPersistence
   )
 
-  val mqttFlow: Source[MqttMessageWithAck, Future[Done]] =
+  val mqttSource: Source[MqttMessageWithAck, Future[Done]] = {
     MqttSource.atLeastOnce(
       connectionSettings
         .withClientId(clientId = "backend-source")
@@ -33,12 +62,28 @@ object Main extends App {
       ),
       bufferSize = 8 // # messages before back-pressure is applied
     )
+  }
 
-  val result = mqttSource
-    .via()
-    .mapAsync(1)(messageWithAck =>
-      messageWithAck.ack().map(_ => messageWithAck.message)
-    )
-    .take(input.size)
-    .runWith(Sink.seq)
+  val mqttSink: Sink[MqttMessage, Future[Done]] =
+    MqttSink(connectionSettings, MqttQoS.AtLeastOnce)
+
+  wrapWithAsRestartSource(mqttSource).runForeach((msg: MqttMessageWithAck) => {
+    msg.ack()
+    val name = msg.message.topic.split("/")(1)
+    system
+      .actorSelection("user/" + name)
+      .resolveOne()
+      .onComplete {
+        case Success(actor) =>
+          actor ! msg
+          println(s"Sending to $name: ${msg.message.payload.toString()}")
+
+        case Failure(ex) =>
+          println(s"Actor name is $name\n")
+          val actor =
+            system.actorOf(Props(classOf[ContactTracingActor], mqttSink), name)
+          actor ! msg
+      }
+  })
+
 }
